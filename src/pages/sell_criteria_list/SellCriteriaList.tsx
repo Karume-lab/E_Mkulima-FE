0e926b5fba369cd8c5d5475a48f4c995d2e54888
Karume-lab <danielkarume@outlook.com>
2024-06-14 23:57:10 +0300

    add sell criteria

--- src/pages/sell_criteria_list/SellCriteriaList.tsx
+++ src/pages/sell_criteria_list/SellCriteriaList.tsx
@@ -1,5 +1,44 @@
+import DivLink from '../../components/DivLink';
+
+
+interface SellCriteriaLink {
+    text: string;
+    link: string
+}
+
+
+const sellCriteriaLinks: SellCriteriaLink[] = [
+    {
+        text: "Location",
+        link: "/dashboard/produce/sell/locations",
+    },
+    {
+        text: "Category",
+        link: "/dashboard/produce/sell/categories",
+    },
+    {
+        text: "Description",
+        link: ""
+    },
+    {
+        text: "Name",
+        link: ""
+    },
+    {
+        text: "Contact",
+        link: "/dashboard/produce/buy/id/message"
+    }
+]
 const SellCriteriaList = () => {
-    return <div>SellCriteriaList</div>;
+    return (
+        <div>
+            {sellCriteriaLinks.map(({ text, link, }, index: number) => {
+                return (
+                    <DivLink route={link} text={text} styling='' key={index} />
+                )
+            })}
+        </div>
+    );
 };
 
 export default SellCriteriaList;
