5c368b903936a1bafacdf41a6a4ef505e9a32e15
Karume-lab <danielkarume@outlook.com>
2024-06-15 01:13:20 +0300

    linked sign up and login pages together

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -9,7 +9,7 @@ const Login = () => {
                 <h1 className="text-2xl font-bold mb-2 text-center">Welcome back</h1>
                 <h2 className="text-lg mb-6 text-center">Login to your account</h2>
                 <form className="w-full flex flex-col gap-3">
-                    <FormField label={"Email"} placeholder={"johndoe@gmail.com"} required={true} styling='p-2 border'/>
+                    <FormField label={"Email"} placeholder={"johndoe@gmail.com"} required={true} styling='p-2 border' />
                     <FormField label={"Password"} placeholder={"********"} required={true} styling='p-2 border' />
                     <div className="flex items-center mb-4">
                         <input type="checkbox" id="rememberMe" className="mr-2" />
@@ -17,6 +17,7 @@ const Login = () => {
                     </div>
                     <DivLink text={"Forgot Password"} route={""} styling="text-lime-500 hover:underline mb-4" />
                     <Button text="Login" styling="bg-lime-500 text-white w-full py-2 rounded hover:scale-105 hover:drop-shadow-lg transition" />
+                    <DivLink text={"Don't have an account? Sign up for one here."} route={"/auth/sign_up"} styling={"text-center bg-lime-500 text-white w-full py-2 rounded hover:scale-105 hover:drop-shadow-lg transition"} />
                 </form>
             </div>
         </div>
--- src/pages/sign_up/SignUp.tsx
+++ src/pages/sign_up/SignUp.tsx
@@ -1,3 +1,5 @@
+import Button from '../../components/Button';
+import DivLink from '../../components/DivLink';
 import FormField from '../../components/FormField';
 const SignUp = () => {
     return (
@@ -9,9 +11,10 @@ const SignUp = () => {
                 <FormField label={'Email'} placeholder={'johndoe@gmail.com'} required={true} styling='mb-4 p-2 border' />
                 <FormField label={'Password'} placeholder={'********'} required={true} styling='mb-4 p-2 border' />
                 <FormField label={'Confirm Password'} placeholder={'********'} required={true} styling='mb-4 p-2 border' />
-                <button type='submit' className="bg-lime-500 text-white w-full py-2 rounded hover:scale-105 hover:drop-shadow-lg transition">
-                    Create Account
-                </button>
+                <div className='flex flex-col gap-2'>
+                    <Button text="Create Account" styling="bg-lime-500 text-white w-full py-2 rounded hover:scale-105 hover:drop-shadow-lg transition" />
+                    <DivLink text={"Already have an account? Log in here."} route={"/auth/sign_up"} styling={"text-center bg-lime-500 text-white w-full py-2 rounded hover:scale-105 hover:drop-shadow-lg transition"} />
+                </div>
             </form>
         </div>
     );
