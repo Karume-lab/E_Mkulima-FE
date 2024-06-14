952ad72ac9bcfc5c88ddebc8c99caf8ff7f9be2c
Karume-lab <danielkarume@outlook.com>
2024-06-15 00:04:53 +0300

    add category

--- src/pages/category_list/CategoryList.tsx
+++ src/pages/category_list/CategoryList.tsx
@@ -1,5 +1,12 @@
+import DivLink from '../../components/DivLink';
+import ProduceList from '../produce_list/ProduceList';
 const Categories = () => {
-    return <div>Categories</div>;
+    return (
+        <>
+            <DivLink route='/dashboard/produce/sell/locations' styling='' text='Locations' />
+            <ProduceList />
+        </>
+    );
 };
 
 export default Categories;
