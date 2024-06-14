d7373bae374733341d6dd963b5fff4e251f0f897
Karume-lab <danielkarume@outlook.com>
2024-06-15 00:56:33 +0300

    fix overflow

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -4,12 +4,12 @@ import Button from "../../components/Button";
 
 const Login = () => {
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-lime-100">
-            <div className="w-full md:w-1/2 bg-white p-8 rounded-lg shadow-md transit">
+        <div className="flex flex-col items-center justify-center bg-lime-100 h-[80%]">
+            <div className="w-full md:w-1/2 bg-white p-8 rounded-lg shadow-md">
                 <h1 className="text-2xl font-bold mb-2">Welcome back</h1>
                 <h2 className="text-lg mb-6">Login to your account</h2>
                 <form className="w-full flex flex-col gap-3">
-                    <FormField label={"Email"} placeholder={"johndoe@gmail.com"} required={true} styling='p-2'/>
+                    <FormField label={"Email"} placeholder={"johndoe@gmail.com"} required={true} styling='p-2' />
                     <FormField label={"Password"} placeholder={"********"} required={true} styling='p-2' />
                     <div className="flex items-center mb-4">
                         <input type="checkbox" id="rememberMe" className="mr-2" />
