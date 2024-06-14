056df97f20ff520b72a4317d89bb64120f0717a4
Karume-lab <danielkarume@outlook.com>
2024-06-14 23:23:47 +0300

    add locations

---
+++ src/MOCK_DATA/locations.ts
@@ -0,0 +1,11 @@
+const locations = [
+    "Kapletundo",
+    "Kamungei",
+    "Ririk",
+    "Kapkoitim",
+    "Chesilyot",
+
+    // ADD MORE LOCATIONS HERE
+]
+
+export default locations;
--- src/pages/location_list/LocationList.tsx
+++ src/pages/location_list/LocationList.tsx
@@ -1,5 +1,19 @@
+import DivLink from "../../components/DivLink";
+import { locations } from "../../MOCK_DATA";
+
 const LocationList = () => {
-    return <div>LocationList</div>;
+	return (
+		<ul>
+			{locations.map((location: string, index: number) => (
+				<DivLink
+					route="/dashboard/produce/buy"
+					styling=""
+					text={location}
+					key={index}
+				/>
+			))}
+		</ul>
+	);
 };
 
 export default LocationList;
